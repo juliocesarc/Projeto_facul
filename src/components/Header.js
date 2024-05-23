c0ca57caac120d1a6d7a66097f78b45b048fc8ce
Lucas <souza.lucasj@gmail.com>
2024-05-23 09:07:41 -0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Header.css"; // Importar o arquivo de estilos
+
+function Header() {
+  const navigate = useNavigate();
+
+  return (
+    <header>
+      <button onClick={() => navigate("/")}>Home</button>
+      <button onClick={() => navigate("/form")}>Form</button>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/pages/Form.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Form() {
+  return (
+    <div>
+      <h1>Página de Formulário</h1>
+    </div>
+  );
+}
+
+export default Form;
